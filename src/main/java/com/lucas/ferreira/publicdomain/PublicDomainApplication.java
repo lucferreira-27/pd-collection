8e3716a78f0d0b6c8d6f4d548f0922e270b8009a
Lucas Ferreira D. Silva <lucferreira14@gmail.com>
2021-03-28 21:16:19 -0300

    first commit

---
+++ src/main/java/com/lucas/ferreira/publicdomain/PublicDomainApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.ferreira.publicdomain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicDomainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicDomainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucas/ferreira/publicdomain/PublicDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.ferreira.publicdomain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicDomainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
